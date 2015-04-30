b4f39464d01ae01030b57e4d21d565016970d0dd
Julius Tarng <julius@tarng.com>
2015-04-29 20:30:45 -0700

    Initial Commit

---
+++ Oculus Rift DK2/Oculus_Rift_DK2PlugIn.m
@@ -0,0 +1,98 @@
+//
+//  Oculus_Rift_DK2PlugIn.m
+//  Oculus Rift DK2
+//
+//  Created by Julius Tarng on 4/29/15.
+//  Copyright (c) 2015 Julius Tarng. All rights reserved.
+//
+
+// It's highly recommended to use CGL macros instead of changing the current context for plug-ins that perform OpenGL rendering
+#import <OpenGL/CGLMacro.h>
+
+#import "Oculus_Rift_DK2PlugIn.h"
+
+#define	kQCPlugIn_Name				@"Oculus Rift DK2"
+#define	kQCPlugIn_Description		@"Oculus Rift DK2 description"
+
+@implementation Oculus_Rift_DK2PlugIn
+
+// Here you need to declare the input / output properties as dynamic as Quartz Composer will handle their implementation
+//@dynamic inputFoo, outputBar;
+
++ (NSDictionary *)attributes
+{
+	// Return a dictionary of attributes describing the plug-in (QCPlugInAttributeNameKey, QCPlugInAttributeDescriptionKey...).
+    return @{QCPlugInAttributeNameKey:kQCPlugIn_Name, QCPlugInAttributeDescriptionKey:kQCPlugIn_Description};
+}
+
++ (NSDictionary *)attributesForPropertyPortWithKey:(NSString *)key
+{
+	// Specify the optional attributes for property based ports (QCPortAttributeNameKey, QCPortAttributeDefaultValueKey...).
+	return nil;
+}
+
++ (QCPlugInExecutionMode)executionMode
+{
+	// Return the execution mode of the plug-in: kQCPlugInExecutionModeProvider, kQCPlugInExecutionModeProcessor, or kQCPlugInExecutionModeConsumer.
+	return kQCPlugInExecutionModeProcessor;
+}
+
++ (QCPlugInTimeMode)timeMode
+{
+	// Return the time dependency mode of the plug-in: kQCPlugInTimeModeNone, kQCPlugInTimeModeIdle or kQCPlugInTimeModeTimeBase.
+	return kQCPlugInTimeModeNone;
+}
+
+- (instancetype)init
+{
+	self = [super init];
+	if (self) {
+		// Allocate any permanent resource required by the plug-in.
+	}
+	
+	return self;
+}
+
+
+@end
+
+@implementation Oculus_Rift_DK2PlugIn (Execution)
+
+- (BOOL)startExecution:(id <QCPlugInContext>)context
+{
+	// Called by Quartz Composer when rendering of the composition starts: perform any required setup for the plug-in.
+	// Return NO in case of fatal failure (this will prevent rendering of the composition to start).
+	
+	return YES;
+}
+
+- (void)enableExecution:(id <QCPlugInContext>)context
+{
+	// Called by Quartz Composer when the plug-in instance starts being used by Quartz Composer.
+}
+
+- (BOOL)execute:(id <QCPlugInContext>)context atTime:(NSTimeInterval)time withArguments:(NSDictionary *)arguments
+{
+	/*
+	Called by Quartz Composer whenever the plug-in instance needs to execute.
+	Only read from the plug-in inputs and produce a result (by writing to the plug-in outputs or rendering to the destination OpenGL context) within that method and nowhere else.
+	Return NO in case of failure during the execution (this will prevent rendering of the current frame to complete).
+	
+	The OpenGL context for rendering can be accessed and defined for CGL macros using:
+	CGLContextObj cgl_ctx = [context CGLContextObj];
+	*/
+	
+	return YES;
+}
+
+- (void)disableExecution:(id <QCPlugInContext>)context
+{
+	// Called by Quartz Composer when the plug-in instance stops being used by Quartz Composer.
+}
+
+- (void)stopExecution:(id <QCPlugInContext>)context
+{
+	// Called by Quartz Composer when rendering of the composition stops: perform any required cleanup for the plug-in.
+}
+
+@end
